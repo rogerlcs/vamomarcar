7ccce1caa3db3af352594892accc351cc8bb9bd8
rogerlcs <rogerleo334@gmail.com>
2021-11-09 12:33:33 -0300

    Primeira versão login

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+
+// connecting to db
+$con = pg_connect(getenv("DATABASE_URL"));
+
+// array for JSON response
+$response = array();
+
+$username = NULL;
+$password = NULL;
+
+// Método para mod_php (Apache)
+if ( isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+    $username = $_SERVER['PHP_AUTH_USER'];
+    $password = $_SERVER['PHP_AUTH_PW'];
+}
+// Método para demais servers
+elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
+    if(preg_match( '/^basic/i', $_SERVER['HTTP_AUTHORIZATION']))
+		list($username, $password) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+}
+
+// Se a autenticação não foi enviada
+if(is_null($username)) {
+    $response["success"] = 0;
+	$response["error"] = "faltam parametros";
+}
+// Se houve envio dos dados
+else {
+    $query = pg_query($con, "SELECT senha FROM usuario WHERE email='$username'");
+
+	if(pg_num_rows($query) > 0){
+		$row = pg_fetch_array($query);
+		if($password == $row['senha']){
+			$response["success"] = 1;
+		}
+		else {
+			// senha ou usuario nao confere
+			$response["success"] = 0;
+			$response["error"] = "usuario ou senha não confere";
+		}
+	}
+	else {
+		// senha ou usuario nao confere
+		$response["success"] = 0;
+		$response["error"] = "usuario ou senha não confere";
+	}
+}
+
+pg_close($con);
+echo json_encode($response);
+?>
