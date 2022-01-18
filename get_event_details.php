ace2638a407608a64ce6d727fdefe33fc8d6d308
rogerlcs <rogerleo334@gmail.com>
2022-01-18 13:44:38 -0300

    Carregando imagem do evento

--- get_event_details.php
+++ get_event_details.php
@@ -51,6 +51,7 @@ if(isset($_GET["idevento"]) && isset($_GET["idusuario"])){
 			$event["data_marcada"] = $row["data_marcada"];
 			$event["endereco"] = $row["endereco"];
 			$event["descricao"] = $row["descricao"];
+			$event["img"] = $row["img"];
 			$event["datas"] = array();
 			$resultdata = pg_query($con, "SELECT dt.*,(SELECT count(*) as votos FROM vota join agenda_do_evento as agenda on(vota.fk_idagenda = agenda.idagenda) where agenda.fk_datas_codigo = dt.codigo),(SELECT CASE WHEN EXISTS (SELECT * FROM vota JOIN agenda_do_evento AS agenda ON(vota.fk_idagenda = agenda.idagenda) WHERE vota.fk_usuario_codigo = $idusuario and agenda.fk_datas_codigo = dt.codigo) THEN 1 ELSE 0 END) FROM datas as dt join agenda_do_evento as agenda on(dt.codigo = agenda.fk_datas_codigo) where agenda.fk_evento_codigo = $idevento");
 			if(pg_num_rows($resultdata) > 0){
